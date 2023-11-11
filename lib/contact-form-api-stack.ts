6c6d532401985e7abc281ce391148c492fd35d00
Justus Brown <justus.c.brown@gmail.com>
2023-11-11 00:10:49 -0800

    Add Slack integration and dotenv package, update dependencies and .gitignore

--- lib/contact-form-api-stack.ts
+++ lib/contact-form-api-stack.ts
@@ -1,19 +1,38 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subs from "aws-cdk-lib/aws-sns-subscriptions";
+import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as chatbot from "aws-cdk-lib/aws-chatbot";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+
+import 'dotenv/config'
+
+import { Construct } from "constructs";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
 
 export class ContactFormApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ContactFormApiQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const contactFormApiTopic = new sns.Topic(this, "ContactFormApiTopic");
+
+    new chatbot.SlackChannelConfiguration(this, "ContactFormSlackChannel", {
+      slackChannelConfigurationName: "ContactFormPortfolioSlackChannel",
+      slackWorkspaceId: process.env.SLACK_WORKSPACE_ID || "",
+      slackChannelId: process.env.CONTACT_FORM_CHANNEL_ID || "",
+      notificationTopics: [contactFormApiTopic],
+      logRetention: RetentionDays.FIVE_DAYS,
+    })
+
+    // make a REST API Gateway
+    const contactFormApi = new apiGateway.RestApi(this, "contact-me", {
+      restApiName: "Contact Form API",
+      description: "This API handles contact form submissions",
     });
+    contactFormApi.root.addMethod("POST");
+  };
 
-    const topic = new sns.Topic(this, 'ContactFormApiTopic');
+  // hook APIGateway to SNS in Chatbot approved format (may require a lambda)
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
 }
